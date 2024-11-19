a129fd89e2e9ed02fe9e75c96000d1fe6090673d
myeongsun <himyeongsun@gmail.com>
2024-11-19 14:38:02 +0900

    241119 최종 수정

--- src/main/java/com/example/shoppro/ShopProApplication.java
+++ src/main/java/com/example/shoppro/ShopProApplication.java
@@ -2,8 +2,10 @@ package com.example.shoppro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ShopProApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/shoppro/repository/MemberRepository.java
+++ src/main/java/com/example/shoppro/repository/MemberRepository.java
@@ -1,11 +1,11 @@
 package com.example.shoppro.repository;
 
 import com.example.shoppro.entity.Item;
+import com.example.shoppro.entity.Member;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.lang.reflect.Member;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
